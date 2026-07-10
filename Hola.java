51ffa270efce8aaa6359245f19ff560cc592bd4d
guillcatandaj-oss <guillcatandaj@gmail.com>
2026-07-10 00:16:16 -0400

    Info update

--- Hola.java
+++ Hola.java
@@ -1,1 +1,12 @@
-# Java
+public class Hola {
+    public static void main(String[] args) {
+        System.out.println("Hola, Vale :)");
+        Scanner scanner = new Scanner(System.in);
+        int edad = scanner.nextInt();
+        system.out.println("Ingresa tu edad: ");
+        int edad=scanner.nextInt();
+        system.out.println("Ingresa tu estatura: ");
+        double estatura=scanner.nextDouble();
+    
+    }
+}
