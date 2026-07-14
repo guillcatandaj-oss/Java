d342c6ee011d972951cc4217094dd4ab076cc9fa
guillcatandaj-oss <guillcatandaj@gmail.com>
2026-07-13 21:37:12 -0400

    New Univ Project

---
+++ CuadradoSimbolos.java
@@ -0,0 +1,10 @@
+public class CuadradoSimbolos {
+    public static void cuadrado(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
